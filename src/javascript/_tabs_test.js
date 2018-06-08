b871adb93428001d723645bf3f4d3da80660822d
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-07 19:23:45 -0700

    cleanup

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -3,19 +3,19 @@
 
 	var assert = require("./assert");
 
-	describe("Elements", function() {
+	describe("Tabs", function() {
 		it("div", function() {
-			var div = document.createElement("div");
 
-			div.innerHTML = "This is an example.";
 
-			document.body.appendChild(div);
 
-			var p = document.createElement("p");
-			p.innerHTML = "This is an embedded paragraph";
-			div.appendChild(p);
 
-			div.parentNode.removeChild(div);
+
+
+
+			// var div = document.createElement("div");
+			// div.innerHTML = "This is an example.";
+			// document.body.appendChild(div);
+			// div.parentNode.removeChild(div);
 		});
 	});
 }());
