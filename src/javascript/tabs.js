9670c78a1c4c2fc25fe7e122585efd8a552c4b18
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-07 19:00:37 -0700

    updated index.html and screen.css

--- src/javascript/tabs.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
-(function() {
-	"use strict";
-
-	exports.initialize = function initialize(element) {
-		element.style.display = "none";
-	};
-
-}());
