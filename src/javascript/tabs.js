92e1b551d64f790a4bc18c4d04ebb0fa6f6cf865
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-09 05:44:23 -0700

    now using classList to set class attribute

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -2,12 +2,7 @@
     "use strict";
 
     exports.initialize = function initialize(element, className) {
-        var classes = element.getAttribute("class");
-
-		if (classes === null) classes = className;
-		else classes += " " + className;
-        
-        element.setAttribute("class", classes);
+        element.classList.add(className);
     };
 
 }());
