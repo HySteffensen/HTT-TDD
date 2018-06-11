cfd38aa488a87c2c7af934084d48da49de5416a5
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-10 22:56:26 -0700

    hides all elements except default tab

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -16,28 +16,24 @@
 			removeElement(container);
 		});
 
-		it("hides multiple elements", function() {
+		it("hides all elements except default tab", function() {
 			var element1 = addElement("div");
-			var element2 = addElement("div");
+			var defaultElement = addElement("div");
 			var element3 = addElement("div");
 
-			tabs.initialize([element1, element2, element3], "hideClass");
-			assert.equal(getClass(element1), "hideClass");
-			assert.equal(getClass(element2), "hideClass");
-			assert.equal(getClass(element3), "hideClass");
-		});
-
-		it("sets a class on an element without previous classes", function() {
-			var element = addElement("div");
-			tabs.initialize([ element ], "someClass");
-			assert.equal(getClass(element), "someClass");
+			tabs.initialize(defaultElement, [element1, defaultElement, element3], "hideClass");
+			
+			assert.equal(getClass(element1), "hideClass", "element 1 should be hidden");
+			assert.equal(getClass(defaultElement), "", "default element should not be hidden");
+			assert.equal(getClass(element3), "hideClass", "element 3 should be hidden");
 		});
 
 		it("sets a class on an element without overwriting existing classes", function() {
-			var element = addElement("div");
-			element.setAttribute("class", "existingClass");
-			tabs.initialize([ element ], "newClass");
-			assert.equal(getClass(element), "existingClass newClass");
+			var defaultElement = addElement("div");
+			var hiddenElement = addElement("div");
+			hiddenElement.setAttribute("class", "existingClass");
+			tabs.initialize(defaultElement, [ defaultElement, hiddenElement ], "newClass");
+			assert.equal(getClass(hiddenElement), "existingClass newClass");
 		});
 
 		function getClass(element) {
--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -1,10 +1,11 @@
 (function() {
     "use strict";
 
-    exports.initialize = function initialize(elementList, className) {
+    exports.initialize = function initialize(defaultElement, elementList, className) {
         elementList.forEach(function(element) {
             element.classList.add(className);
         });
+        defaultElement.classList.remove(className);
     };
 
 }());
