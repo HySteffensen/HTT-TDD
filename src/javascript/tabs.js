371845cbb558e5a57e5df4bd8ff2a53da814eabf
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-12 12:00:18 -0700

    factored out showTab() function

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -2,41 +2,36 @@
     "use strict";
     
     exports.initialize = function initialize(options) {
-        var tabs = options.tabs;
-        var content = options.content;
-        var defaultTab = options.defaultTab;
-        var activeTabClass = options.activeTabClass;
-        var hiddenContentClass = options.hiddenContentClass;
         
-        checkOption(tabs, "option.tabs");
-        checkOption(content, "option.content");
-        checkOption(defaultTab, "option.defaultTab");
-        checkOption(activeTabClass, "option.activeTabClass");
-        checkOption(hiddenContentClass, "option.hiddenContentClass");
-        
-        showTab(defaultTab, tabs, content, activeTabClass, hiddenContentClass);
+        checkOption(options.tabs, "option.tabs");
+        checkOption(options.content, "option.content");
+        checkOption(options.defaultTab, "option.defaultTab");
+        checkOption(options.activeTabClass, "option.activeTabClass");
+        checkOption(options.hiddenContentClass, "option.hiddenContentClass");
+
+        showTab(options.defaultTab, options);
     };
-
-        function showTab(tabToShow, tabs, content, activeTabClass, hiddenContentClass) {
-            var activeIndex = findIndexOfDefaultElement(tabs, tabToShow);
-            var defaultContent = content[activeIndex];
     
-            content.forEach(function(element) {
-                element.classList.add(hiddenContentClass);
-            });
-            defaultContent.classList.remove(hiddenContentClass);
-            tabToShow.classList.add(activeTabClass);
-        }
-        
-        function findIndexOfDefaultElement(contentTabs, defaultContentTab) {
-            for (var i = 0; i < contentTabs.length; i++) {
-                if (contentTabs[i] === defaultContentTab) return i;
-            }
-            throw new Error("Could not find default tab in list");
-        }
+    function showTab(tabToShow, options) {
+        var activeIndex = findIndex(options.tabs, tabToShow);
+        var contentToShow = options.content[activeIndex];
 
-        function checkOption(option, name) {
-            if (option === undefined) throw new Error("Expected " + name);
+        options.content.forEach(function(element) {
+            element.classList.add(options.hiddenContentClass);
+        });
+        contentToShow.classList.remove(options.hiddenContentClass);
+        tabToShow.classList.add(options.activeTabClass);
+    }
+    
+    function findIndex(contentTabs, defaultContentTab) {
+        for (var i = 0; i < contentTabs.length; i++) {
+            if (contentTabs[i] === defaultContentTab) return i;
         }
+        throw new Error("Could not find default tab in list");
+    }
+
+    function checkOption(option, name) {
+        if (option === undefined) throw new Error("Expected " + name);
+    }
 
 }());
